fb3a63cb575e3f8c255a9b14490c075304ca6501
yutahonma <yuta.honma@rakus.co.jp>
2017-08-24 10:43:21 +0900

    add getter & setter

--- src/main/java/jp/co/rakus/BranchSamplePart2/Car.java
+++ src/main/java/jp/co/rakus/BranchSamplePart2/Car.java
@@ -9,4 +9,16 @@ public class Car {
 	//getter/setterをつけておいてください
 	private int speed;
 	private String bodyColor;
+	public int getSpeed() {
+		return speed;
+	}
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	public String getBodyColor() {
+		return bodyColor;
+	}
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
 }
