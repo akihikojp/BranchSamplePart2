700b0c1be1b599bd3c42cdf535b2780e03b38644
yahiro <akihiko.yahiro@rakus.co.jp>
2017-08-24 10:37:44 +0900

    Carクラスを作成しました。

---
+++ src/main/java/jp/co/rakus/BranchSamplePart2/Car.java
@@ -0,0 +1,12 @@
+package jp.co.rakus.BranchSamplePart2;
+
+/**
+ * Carクラス
+ * @author akihiko.yahiro
+ *
+ */
+public class Car {
+	//getter/setterをつけておいてください
+	private int speed;
+	private String bodyColor;
+}
